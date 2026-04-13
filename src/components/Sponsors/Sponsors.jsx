c77d223a3188610ab57a5a57e84dab39b3762fae
noorelian <nelian1@student.ohlone.edu>
2026-04-13 13:08:45 -0700

    Updated sponsors logos and links.

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -8,15 +8,15 @@ import "./Sponsors.css";
 function Sponsors() {
     const sponsors = [
         { name: "ASOC", logo: ASOCLogo, link: "https://www.asocohlone.org/" },
-        { name: "NordPass", logo: "https://images.saasworthy.com/nordpass_6669_logo_1719204895_2ykhq.png", link: "https://nordpass.com" },
+        { name: "NordPass", logo: "https://upload.wikimedia.org/wikipedia/commons/4/4a/Nordpass.png", link: "https://nordpass.com" },
         { name: "NordProtect", logo: "https://res.cloudinary.com/nordsec/image/upload/q_auto,f_auto/v1/nord-security-web/homepage/2025/Products/nordprotect.png", link: "https://nordprotect.com/" },
         { name: "AoPS", logo: AOPSLogo, link: "https://artofproblemsolving.com/" },
         { name: "Balsamiq", logo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjgswaEaC8rEvxwP14reSkjSMERq34aLAFtQ&s", link: "https://balsamiq.com/"},
-        { name: "NordVPN", logo: "https://play-lh.googleusercontent.com/G166NGtIsLp_UdpDjC1FF6dooIgd83fs2lstSI7mhW06q7gmiE1cw4BZ16OCX0WokPE", link: "https://nordvpn.com" },
+        { name: "NordVPN", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/NordVPN_logo.svg/3840px-NordVPN_logo.svg.png", link: "https://nordvpn.com/hackathons" },
         { name: "InterviewCake", logo: "https://www.assertnotmagic.com/img/interview-cake.jpg", link: "https://www.interviewcake.com" },
         { name: "1Password", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/1Password_wordmark_blue_2023.svg/3840px-1Password_wordmark_blue_2023.svg.png", link: "https://1password.com" },
-        { name: "Saily", logo: "https://cdn.cheapoguides.com/wp-content/uploads/sites/2/2024/02/Saily-logo-770x422.png", link: "https://saily.com"},
-        { name: "Incogni", logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdhvUze9qyZi_wylK2rZKdUdxTTLBLTW7_vg&s", link: "https://incogni.com" },
+        { name: "Saily", logo: "https://sb.nordcdn.com/m/4c608a2b46c0059c/original/saily-logo-yellow.svg", link: "https://saily.com"},
+        { name: "Incogni", logo: "https://upload.wikimedia.org/wikipedia/commons/5/53/Incogni.com_logo.png", link: "https://incogni.com" },
         { name: "nexos.ai", logo: "https://lever-client-logos.s3.us-west-2.amazonaws.com/85f55f08-16c4-45eb-8455-e8978a378db4-1743516114537.png", link: "https://nexos.ai/" },
         { name: "MarchUp", logo: marchUpLogo, link: "https://marchup.net" }
        /* { name: "CodeForCause", logo: codeforcauseLogo, link: "https://www.codeforcause.dev/"},
